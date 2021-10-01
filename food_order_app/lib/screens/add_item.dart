7b5a78422b557804cd00c312b4bf082ea2bd2f0f
Saiful Islam <saiful@cslsoft.com>
2021-10-01 12:12:10 +0600

    Form Validation

--- food_order_app/lib/screens/add_item.dart
+++ food_order_app/lib/screens/add_item.dart
@@ -16,22 +16,40 @@ class _AddItemState extends State<AddItem> {
   String xyz;
   Foods foodList;
 
+  final _keyForm = GlobalKey<FormState>();
+
   final itemNameController = TextEditingController();
   final itemDescriptionController = TextEditingController();
   final itemPriceController = TextEditingController();
   final itemUrlController = TextEditingController();
 
   void saveItemToList(){
+
     print('saveItemToList ${itemNameController.text}');
 
-    Food food =  Food(
-      title: itemNameController.text,
-      description: itemDescriptionController.text,
-      price: double.parse(itemPriceController.text),
-      imageUrl:itemUrlController.text,
-    );
+    if(_keyForm.currentState.validate()){
+      Food food =  Food(
+        title: itemNameController.text,
+        description: itemDescriptionController.text,
+        price: double.parse(itemPriceController.text),
+        imageUrl:itemUrlController.text,
+      );
 
-    foodList.addFood(food);
+      foodList.addFood(food);
+
+      itemNameController.text = '';
+      itemDescriptionController.text = '';
+      itemPriceController.text = '';
+      itemUrlController.text = '';
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Successfully Saved Item')));
+
+
+    }
+
+
+
+    /**/
 
   }
 
@@ -45,14 +63,21 @@ class _AddItemState extends State<AddItem> {
       body: Padding(
         padding: const EdgeInsets.only(left: 16.0,right: 16.0,top: 10,bottom: 10),
         child: Form(
+          key: _keyForm,
           child: ListView(
             children: [
-
               TextFormField(
                 controller: itemNameController,
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.next,
                 onChanged: (v){xyz = v;},
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter item name';
+                  }else{
+                    return null;
+                  }
+                },
                 decoration: InputDecoration(
                   hintText: 'Enter Item Name',
                   hintStyle: TextStyle(
@@ -69,6 +94,13 @@ class _AddItemState extends State<AddItem> {
                 controller: itemDescriptionController,
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.next,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter item description';
+                  }else{
+                    return null;
+                  }
+                },
                 decoration: InputDecoration(
                     hintText: 'Enter Item Description',
                     hintStyle: TextStyle(
@@ -85,6 +117,20 @@ class _AddItemState extends State<AddItem> {
                 controller: itemPriceController,
                 keyboardType: TextInputType.number,
                 textInputAction: TextInputAction.next,
+                //maxLength: 11,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter item price';
+                  }else{
+                    /*if(value.length == 11){
+                      return null;
+                    }else{
+                      return 'Please enter 11 digit mobile number';
+                    }*/
+
+                    return null;
+                  }
+                },
                 decoration: InputDecoration(
                     hintText: 'Enter Item Price',
                     hintStyle: TextStyle(
@@ -101,6 +147,13 @@ class _AddItemState extends State<AddItem> {
                 controller: itemUrlController,
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.done,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter item url';
+                  }else{
+                    return null;
+                  }
+                },
                 decoration: InputDecoration(
                     hintText: 'Enter Item URL',
                     hintStyle: TextStyle(
